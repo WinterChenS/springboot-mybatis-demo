835cafa0d966e6638eb02472a2cff8b677a62d20
WinterChenS <1085143002@qq.com>
2018-03-02 17:06:15 +0800

    修复生成代码的bug

---
+++ src/main/java/com/winter/mapper/RoleMapper.java
@@ -0,0 +1,17 @@
+package com.winter.mapper;
+
+import com.winter.model.Role;
+
+public interface RoleMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    Role selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
---
+++ src/main/java/com/winter/model/Role.java
@@ -0,0 +1,33 @@
+package com.winter.model;
+
+public class Role {
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type == null ? null : type.trim();
+    }
+}
